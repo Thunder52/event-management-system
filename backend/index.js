c75b6ece49d6b35ba6c19b305e7c9c3ac0ca8201
Husain Ali <husainali7865253@gmail.com>
2025-12-26 22:46:05 +0530

    fix cors error

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173','event-management-system-rho-two.vercel.app'],
+    origin: 'event-management-system-rho-two.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
